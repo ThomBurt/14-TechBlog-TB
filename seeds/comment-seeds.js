9633d2330b48ec5195ce0cf80474541cbc516755
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-03-03 16:42:12 -0500

    homepage loading without errors but is not populated

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,7 +3,6 @@ const { Comment } = require('../models');
 const commentData = [
   {
     description: "Agreed. This is my first comment too.",
-    date_created: "04/04/21",
     post_id: 1,
     user_id: 2
   }
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,7 +4,6 @@ const postData = [
   {
     title: "First Blog Post",
     description: "Hello, this is my first blog post. I am excited to see it show up on this page.",
-    date_created: "04/04/21",
     user_id: 1
   }
 ];
