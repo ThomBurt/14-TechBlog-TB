cd1de9d7995ae5ebd1709007c02798470e6fc7ec
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-03-04 15:28:22 -0500

    not allowing login

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,15 +3,15 @@ const signupFormHandler = async (event) => {
     event.preventDefault();
 
     // Gather the data from the form elements on the page
-    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && email && password) {
+    if (username && email && password) {
         // Send the e-mail and password to the server
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
